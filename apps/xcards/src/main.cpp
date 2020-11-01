927eb4c0604abbd37ac100e1b8957cf69f2fbda8
Philip Wenkel <philip.sve@hotmail.com>
2020-11-01 20:36:01 +0100

    a

--- apps/xcards/src/main.cpp
+++ apps/xcards/src/main.cpp
@@ -1,13 +1,12 @@
 #include "xcards/xcards.hpp"
-#include <ph/time/date/Date.hpp>
-#include <ph/time/timer/Timer.hpp>
+// #include <ph/time/date/Date.hpp>
+// #include <ph/time/timer/Timer.hpp>
 
 int main(int argc, char** argv)
 {
-      ph::time::Timer t("hej");
-      ph::time::Date d;
+      // ph::time::Timer t("hej");
+      // ph::time::Date d;
       // xcards app2 (argc, argv);
-      
       
       
       
--- libs/ph/libs/time/libs/timer/include/ph/time/timer/Timer.hpp
+++ libs/ph/libs/time/libs/timer/include/ph/time/timer/Timer.hpp
@@ -4,6 +4,9 @@
 using namespace std;
 using namespace std::chrono;
 namespace ph::time{
+
+
+      
 class Timer {
 public:
       
