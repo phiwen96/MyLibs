eea9687db5fa7e0fda30df9ba2fc1617c227e4b5
Philip Wenkel <philip.sve@hotmail.com>
2020-11-03 14:50:59 +0100

    a

--- libs/ph/libs/math/include/ph/math/math.hpp
+++ libs/ph/libs/math/include/ph/math/math.hpp
@@ -3,19 +3,80 @@ namespace ph::math
 {
 
 
+//template <typename T, typename enable_if<is_arithmetic<T>::value, T>::type = 0>
+//struct Number
+//{
+//
+//      Number (const T& number) : m_number (number)
+//      {
+//
+//      }
+//
+//private:
+//       T m_number;
+//};
 
 
+template <typename... T>
+struct Vector
+{
+      Vector (const T&... t) : m_vec (t...), m_size (sizeof...(T))
+      {
+            
+      }
+      
+      
+      
+private:
+      tuple<T...> m_vec;
+      size_t m_size;
+};
 
 
 
 
 
+struct BaseConstructor { BaseConstructor(int=0) {} };
+class RealNumber; class Complex;
 
+class Number {
+      friend class RealNumber; friend class Complex;
+public:
+      Number ();
+      Number & operator = (const Number &n);
+      Number (const Number &n); virtual ~Number();
+      virtual Number operator + (Number const &n) const; void swap (Number &n) throw ();
+      static Number makeReal (double r);
+      static Number makeComplex (double rpart, double ipart);
+protected:
+      Number (BaseConstructor);
+private:
+      void redefine (Number *n);
+      virtual Number complexAdd (Number const &n) const; virtual Number realAdd (Number const &n) const;
+      Number *rep;
+      short referenceCount;
+};
 
 
 
+class Complex : public Number {
+public:
+      friend class RealNumber; friend class Number;
 
+      Complex (double d, double e); Complex (const Complex &c); virtual ~Complex ();
+      virtual Number operator + (Number const &n) const; virtual Number realAdd (Number const &n) const; virtual Number complexAdd (Number const &n) const;
+      double rpart, ipart;
+};
+class RealNumber : public Number {
+public:
+      friend class Complex; friend class Number;
 
+      RealNumber (double r);
+      RealNumber (const RealNumber &r); virtual ~RealNumber ();
+      virtual Number operator + (Number const &n) const;
+      virtual Number realAdd (Number const &n) const; virtual Number complexAdd (Number const &n) const;
+      double val;
+};
 
 
 
@@ -90,4 +151,15 @@ namespace ph::math
 
 
 
+
+
+
+}
+
+
+namespace std
+{
+template <>
+void swap (ph::math::Number & n1, ph::math::Number & n2) {
+      n1.swap (n2); }
 }
--- libs/ph/libs/math/test/src/main.cpp
+++ libs/ph/libs/math/test/src/main.cpp
@@ -1,13 +1,21 @@
 #include <test/main.hpp>
+#include <ph/math/math.hpp>
 
+using namespace ph::math;
 
 int main(int argc, char** argv)
 {
        
-
+//      Number n(6);
+//      cout << is_arithmetic<decltype(n)>::value << endl;
    
+      Vector v(1, 2, 3);
       
       
+     
+      Number n1 = Complex (1, 2); // Label n1 for a complex number
+      Number n2 = RealNumber (10); // Label n2 for a real number
+      Number n3 = n1 + n2; // Result of addition is labelled n3 Number n2 = n3; // Re-labelling
       
       // constexpr auto s2 = Size<int, char>::value;
     return 0;
